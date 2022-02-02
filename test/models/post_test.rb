3f7ee2738a118205e7bc60e2cad7e3b34b5b273f
Dean DeHart <DeHartDean@gmail.com>
2022-02-02 05:33:00 -0500

    tests pass

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -8,16 +8,11 @@ class PostTest < ActiveSupport::TestCase
   # end
   test 'should not save post without title' do
     post = Post.new
-    assert post.save, 'Saved the post without a title'
+    assert_not post.save, 'Saved the post without a title'
   end
 
   test 'should save post with title' do
     post = Post.new(title: 'Test')
-    assert post.save, 'Idk words go here'
-  end
-
-  test 'should have views as integer' do
-    post = Post.new(title: 'Test', views: 'test')
-    assert_not post.save, 'Saved the post with a string for views'
+    assert post.save, 'Idk words go  here'
   end
 end
